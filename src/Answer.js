d84302e97c2647b29f58c5e50fe8cb9656bb12ba
Oli Bullen <d33con@gmail.com>
2018-04-24 16:42:22 +0100

    typography / button colour

--- src/Answer.js
+++ src/Answer.js
@@ -1,15 +1,25 @@
 import React from "react";
 import PropTypes from "prop-types";
+import Typography from "material-ui/Typography";
 import Button from "material-ui/Button";
 
 const Answer = props => {
   return (
     <div className={props.showAnswer ? "answer shown" : "answer hidden"}>
-      <div>{props.correct ? "Correct!" : "Wrong!"}</div>
+      <Typography variant="display3" gutterBottom>
+        {props.correct ? "Correct!" : "Wrong!"}
+      </Typography>
       {props.correct ? null : (
-        <div>The correct answer is: {props.correctAnswer}</div>
+        <Typography variant="display1" gutterBottom>
+          The correct answer is: {props.correctAnswer}
+        </Typography>
       )}
-      <Button variant="raised" onClick={props.newQuestion}>
+      <Button
+        variant="raised"
+        color="secondary"
+        size="large"
+        onClick={props.newQuestion}
+      >
         Next Question
       </Button>
     </div>
